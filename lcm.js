19c00da28169b451370185a27a7b091eea0f2040
matnex-mix <jolaoshobatmat@gmail.com>
2020-06-24 21:43:31 +0100

    Initial

---
+++ lcm.js
@@ -0,0 +1,28 @@
+/*
+ * REM: A program to find the lcm of two numbers
+ *
+ */
+
+function lcm( number_1, number_2 ){
+
+  var max = number_1*number_2; // The maximum value for the LCM i.e (value1 * value2)
+  var num_1_multiples = []; // Container for the multiples of (value1)
+  var num_2_multiples = []; // Container for the multiples of (value2)
+
+  for( var i=1; i>0; i++ ){ // Create a no-end loop that increments by (1) to serve as our multiple generator
+    multiple_1 = number_1*i; // Calculate a new multiple e.g when i=1; multiple_1=value1*1
+    multiple_2 = number_2*i; // Same as above for (value2)
+
+    if( num_1_multiples.indexOf(multiple_2) != -1 ){ // Check if the new multiple of (value1) is already in the multiples of (value2)
+      return multiple_2; // Return the multiple because it's the first number that will appear as a multiple of both numbers
+    } else if( num_2_multiples.indexOf(multiple_1) != -1 ){ // Check if the new multiple of (value2) is already in the multiples of (value1)
+      return multiple_1; // Return the multiple
+    } else if( multiple_1==max || multiple_2==max ){ // When we reach the maximum value for the LCM i.e (value1*value2)
+      return max;
+    }
+
+    num_1_multiples.push( multiple_1 );
+    num_2_multiples.push( multiple_2 );
+  }
+
+}
