754a7171ecafce2450cd3b48e599a54b7fcd92ef
matnex-mix <jolaoshobatmat@gmail.com>
2020-06-24 21:50:51 +0100

    Update

--- lcm.js
+++ lcm.js
@@ -10,9 +10,9 @@ function lcm( number_1, number_2 ){
   var num_2_multiples = []; // Container for the multiples of (value2)
 
   for( var i=1; i>0; i++ ){ // Create a no-end loop that increments by (1) to serve as our multiple generator
-    multiple_1 = number_1*i; // Calculate a new multiple e.g when i=1; multiple_1=value1*1, when i=2; multiple_1=value1*2,
+    var multiple_1 = number_1*i; // Calculate a new multiple e.g when i=1; multiple_1=value1*1, when i=2; multiple_1=value1*2,
     // if (value1) was 3 then; when i=1; multiple_1=3*1=3, when i=2; multiple_1=3*2=6, where {3, 6} are multiples of 3
-    multiple_2 = number_2*i; // Same as above for (value2)
+    var multiple_2 = number_2*i; // Same as above for (value2)
 
     if( num_1_multiples.indexOf(multiple_2) != -1 ){ // Check if the new multiple of (value1) is already in the multiples of (value2)
       return multiple_2; // Return the multiple because it's the first number that will appear as a multiple of both numbers
