5be140e5cc3f4f3acc664d25e45844375af0fa1c
matnex-mix <jolaoshobatmat@gmail.com>
2020-06-24 21:48:56 +0100

    Initial

--- lcm.js
+++ lcm.js
@@ -10,7 +10,8 @@ function lcm( number_1, number_2 ){
   var num_2_multiples = []; // Container for the multiples of (value2)
 
   for( var i=1; i>0; i++ ){ // Create a no-end loop that increments by (1) to serve as our multiple generator
-    multiple_1 = number_1*i; // Calculate a new multiple e.g when i=1; multiple_1=value1*1
+    multiple_1 = number_1*i; // Calculate a new multiple e.g when i=1; multiple_1=value1*1, when i=2; multiple_1=value1*2,
+    // if (value1) was 3 then; when i=1; multiple_1=3*1=3, when i=2; multiple_1=3*2=6, where {3, 6} are multiples of 3
     multiple_2 = number_2*i; // Same as above for (value2)
 
     if( num_1_multiples.indexOf(multiple_2) != -1 ){ // Check if the new multiple of (value1) is already in the multiples of (value2)
@@ -18,11 +19,11 @@ function lcm( number_1, number_2 ){
     } else if( num_2_multiples.indexOf(multiple_1) != -1 ){ // Check if the new multiple of (value2) is already in the multiples of (value1)
       return multiple_1; // Return the multiple
     } else if( multiple_1==max || multiple_2==max ){ // When we reach the maximum value for the LCM i.e (value1*value2)
-      return max;
+      return max; // Return the maximum
     }
 
-    num_1_multiples.push( multiple_1 );
-    num_2_multiples.push( multiple_2 );
+    num_1_multiples.push( multiple_1 ); // Add (multiple_1) as a multiple of (value1)
+    num_2_multiples.push( multiple_2 ); // Add (multiple_2) as a multiple of (value2)
   }
 
 }
